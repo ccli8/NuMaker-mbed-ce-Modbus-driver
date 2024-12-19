aeb2ecf595dd28ac1fd4d74f4d67fad28ba6e184
Chun-Chieh Li <ccli8@nuvoton.com>
2024-12-19 15:08:53 +0800

    Migrate to Mbed CE
    1.  Configurable for porting (mbed_lib.json5)
        (1) Provide default serial port
        (2) Enable serial RS485 mode
    2.  Mercurial export to git
        (1) Fix line ending ^M issue e.g. git diff

--- portserial.cpp
+++ portserial.cpp
@@ -38,7 +38,7 @@ static void prvvUARTRxISR( void );
 static void prvvUARTISR( void );
 
 /* ----------------------- System Variables ---------------------------------*/
-#if defined(MBED_CONF_APP_DEF_RS485_PORT) && (MBED_CONF_APP_DEF_RS485_PORT)// mbed serial port
+#if MBED_CONF_NU_MODBUS_ENABLE_SERIAL_RS485
     #include "nvt_rs485.h"
     // RS485 TX, RX, RTS pins
     #if defined(TARGET_NUMAKER_PFM_NUC472)  // for NUC472 board
@@ -59,13 +59,13 @@ static void prvvUARTISR( void );
 #else
     //UART TX, RX
     #if defined(TARGET_NUMAKER_PFM_NUC472)  // for NUC472 board
-    Serial pc(PG_2, PG_1);
+    UnbufferedSerial pc(PG_2, PG_1);
     #elif defined(TARGET_NUMAKER_PFM_M453)  // for M453 board
-    Serial pc(PD_1, PD_6);    
+    UnbufferedSerial pc(PD_1, PD_6);
     #elif defined(TARGET_NUMAKER_PFM_M487)  // for M487 board
-    Serial pc(PC_12, PC_11);
+    UnbufferedSerial pc(PC_12, PC_11);
     #elif defined(TARGET_NUMAKER_IOT_M467)  // for M467 board
-    Serial pc(PC_12, PC_11);
+    UnbufferedSerial pc(PC_12, PC_11);
     #else
         #error "The demo code can't be executed on this board."    
     #endif
@@ -107,7 +107,7 @@ BOOL
 xMBPortSerialInit( UCHAR ucPORT, ULONG ulBaudRate, UCHAR ucDataBits, eMBParity eParity )
 {
     pc.baud(ulBaudRate);
-#if defined(MBED_CONF_APP_DEF_RS485_PORT) && (MBED_CONF_APP_DEF_RS485_PORT) // mbed serial port
+#if MBED_CONF_NU_MODBUS_ENABLE_SERIAL_RS485
     #if defined(TARGET_NUMAKER_PFM_NUC472)      // for NUC472 board
     pc.set_rs485_mode(PF_11);
     #elif defined(TARGET_NUMAKER_PFM_M453)  // for M453 board
@@ -191,5 +191,3 @@ static void prvvUARTRxISR( void )
     vMBPortTimersDisable();
     pxMBFrameCBByteReceived(  );
 }
-
-
